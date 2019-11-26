40f292b3d190975bd9f9a167b7bf0fed16925882
Semir Umut Kurt <fatih.ek96@hotmail.com>
2019-11-26 19:01:08 +0300

    first commit of spring boot mvc pet clinic app

---
+++ src/main/java/com/sumutella/petclinic/SpringBootMvcPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.sumutella.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMvcPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMvcPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sumutella/petclinic/SpringBootMvcPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumutella.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMvcPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
