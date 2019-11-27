b1e1c30b478c7e0b749a4de7295ae33e6902dace
Semir Umut Kurt <fatih.ek96@hotmail.com>
2019-11-27 16:03:34 +0300

    owner index page and owner controller has been added. Closes #13

--- pet-clinic-web/src/main/java/com/sumutella/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/sumutella/petclinic/controllers/VetController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class VetController {
 
-    @RequestMapping({"","/", "/index", "/index.html" })
+    @RequestMapping({"/", "/index", "/index.html" })
     public String listVets(){
         return "vets/index";
     }
